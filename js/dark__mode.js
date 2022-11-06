703be0d243bb16f675b6b99a7e7207da73776e63
Tohirbek <tohiruktamov55@gmail.com>
2022-11-06 21:29:49 +0500

    x

---
+++ js/dark__mode.js
@@ -0,0 +1,7 @@
+function toggleDarkLight() {
+    let body = document.getElementById("dark");
+    var currentClass = body.className;
+    dark.className = currentClass == "dark-mode" ?"light-mode"  : "dark-mode";
+  }
+
+
